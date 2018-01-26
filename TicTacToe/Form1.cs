58a749d68249c3f16b48b5d02cf2a58f09cc16cb
MRummanHasan <mrummanhasan@gmail.com>
2018-01-27 02:49:04 +0500

    my first game using C#

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // true=X turn, false=Y turn
+        int turn_count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("My name is Rumman, and this is PF","Ye ABOUT he");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button b =(Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+            turn = !turn;
+            b.Enabled = false;
+            CheckForWinner();
+        }
+        private void CheckForWinner()
+        {
+            bool there_is_Winner = false;
+            if((A1.Text == A2.Text)&&(A2.Text == A3.Text)&&(!A1.Enabled))
+                    { there_is_Winner = true; }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                    { there_is_Winner = true; }
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                    { there_is_Winner = true; }
+
+            if(there_is_Winner)
+            {
+                string winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show(winner+" Win");
+
+            }
+        }
+    }
+}
